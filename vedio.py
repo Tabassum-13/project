65f800330212585aad531ec755f7296c59e72177
Tabassum shaik <111267114+Tabassum-13@users.noreply.github.com>
2024-06-05 19:22:30 +0530

    Update vedio.py

--- vedio.py
+++ vedio.py
@@ -148,7 +148,7 @@ def get_youtube_video_details(video_id, api_key):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        if "items" in data and len(data["items"]) > 0:
+        if "items" in data and len(data["items"]) > 0):
             snippet = data["items"][0]["snippet"]
             title = snippet["title"]
             thumbnail_url = snippet["thumbnails"]["high"]["url"]
